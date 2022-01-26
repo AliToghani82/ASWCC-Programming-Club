8f5070a5c0c70aa7f10b16c1911ba84b55f33e37
AliToghani82 <33048159+AliToghani82@users.noreply.github.com>
2022-01-26 12:34:30 -0800

    main

---
+++ MasterGit.java
@@ -0,0 +1,9 @@
+public class MasterGit {
+   public static void main(String[] args) {
+      System.out.println("Type something here, whatever you like");
+      System.out.println("Type something here, whatever you like");
+      for(int i = 0; i < 2; i++) {
+         System.out.println("Type something here, whatever you like");
+      }
+   } 
+}
