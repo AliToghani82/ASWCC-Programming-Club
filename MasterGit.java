9b2184aaaf742b54097103da0759890db068e4a9
GTeref <gterefi6195@student.whatcom.edu>
2022-01-26 12:42:44 -0800

    added some messages

--- MasterGit.java
+++ MasterGit.java
@@ -1,9 +1,11 @@
+import java.util.*;
+
 public class MasterGit {
    public static void main(String[] args) {
-      System.out.println("Type something here, whatever you like");
-      System.out.println("Type something here, whatever you like");
+      System.out.println("GTeref was here!");
+      System.out.println("GTeref was here!");
       for(int i = 0; i < 2; i++) {
-         System.out.println("Type something here, whatever you like");
+         System.out.println("GTeref was here!");
       }
    } 
 }
